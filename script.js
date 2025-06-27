35f059f709b4a37b79388f71e6af4f2ddfc1c58c
Kshitij-Taywade <shitutaywade345@gmail.com>
2025-06-27 19:32:00 +0530

    certificates

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert("Kshitij Says ., \n  For mobile user , If you want to see the certificates then first you need to copy the certificate text then two option is open , then you choose any one from two option and see which certificate you want to see.")
+// alert("Kshitij Says ., \n  For mobile user , If you want to see the certificates then first you need to copy the certificate text then two option is open , then you choose any one from two option and see which certificate you want to see.")
+
+
+let certificate = document.querySelector("#certificate");
+let certificateDiv = document.querySelector(".certificates-div");
+certificate.addEventListener("click", () => {
+    certificateDiv.style.display = "block";
+})
